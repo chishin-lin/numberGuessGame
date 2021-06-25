45af6c571e9d247e0d235254a17aa1a0545bf39d
lily <chishin004@gmail.com>
2021-06-25 18:56:25 +0800

    project.game

--- numberGame.js
+++ numberGame.js
@@ -1,4 +1,25 @@
 //answer 0~99
-let answer = math.floor(math.ramdom() * 100);
-console.log(answer);
-//prompt("遊戲開始，請輸入數字：");
+let answer = Math.floor(Math.random() * 100);
+
+let n1 = 0;
+let n2 = 99;
+while (true) {
+  let guess = prompt(
+    `Make your guess:(the Number is between : ${n1} ~ ${n2}).`
+  );
+  //超過99或小於0就提釋字窗，並繼續執行此迴圈(使用continue)
+  if (guess < n1 || guess > n2) {
+    alert("Please make a valid input.");
+    continue;
+  }
+
+  if (guess == answer) {
+    alert(`The Answer is ${answer} !`);
+    alert("You win the game!");
+    break;
+  } else if (guess <= answer) {
+    n1 = guess;
+  } else if (guess > answer) {
+    n2 = guess;
+  }
+}
